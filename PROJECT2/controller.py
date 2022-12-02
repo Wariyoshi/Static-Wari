9fee8d5025fdb3efbb5dc6e30038ae341a0f53e6
jacobfrisbie <jacobfrisbie@outlook.com>
2022-12-02 13:32:09 -0600

    Additional Logic Updates
    Additional logic commit

--- PROJECT2/controller.py
+++ PROJECT2/controller.py
@@ -7,7 +7,7 @@ QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
 
 class Controller(QMainWindow, Ui_MainWindow):
-    balance = 100
+    balance = int(100)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -16,15 +16,47 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.cardNumberMiddle.setText('')
         self.cardNumberBottom.setText('')
         self.errorLabel.setText('Choose High or Low\nLow (1-5) High (6-10)')
-        self.LowButton.clicked.connect(lambda: self.playGame())
-        self.HighButton.clicked.connect(lambda: self.playGame())
+        self.LowButton.clicked.connect(lambda: self.playLow())
+        self.HighButton.clicked.connect(lambda: self.playHigh())
+        self.resetButton.clicked.connect(lambda: self.reset())
 
-    def playGame(self):
-        balance = int(100)
+    def playLow(self, balance):
+        # balance = int(100)
+        self.balanceCounter.setText(f'test {balance}')
+        number = randint(1, 10)
+        if self.LowButton.clicked:
+            if number > 5:
+                self.winOrLoseLabel.setText(f'Sorry, you guessed wrong!\nThe number was {number}')
+                balance = balance - 10
+                self.balanceCounter.setText(f'{balance}')
+                if balance < 1:
+                    self.errorLabel.setText('You have a balance of 0\nClick reset to play again')
+            if number < 6:
+                self.winOrLoseLabel.setText('Congratulations, you won!\nClick High or Low to play again')
+                balance = balance + 10
+                self.balanceCounter.setText(f'{balance}')
+                if balance < 1:
+                    self.errorLabel.setText('You have a balance of 0\nClick reset to play again')
+
+    def playHigh(self, balance):
+        # balance = int(100)
         self.balanceCounter.setText(f'{balance}')
-        while balance > 0:
-            number = randint(1, 10)
-            if self.LowButton.clicked:
-                if number > 5:
-                    self.winOrLoseLabel.setText('Congratulations, you won!\nClick High or Low to play again')
-                    balance = balance + 10
+        number = randint(1, 10)
+        if number > 5:
+            self.winOrLoseLabel.setText('Congratulations, you won!\nClick High or Low to play again')
+            balance = balance + 10
+            self.balanceCounter.setText(f'{balance}')
+            if balance < 1:
+                self.errorLabel.setText('You have a balance of 0\nClick reset to play again')
+        if number < 6:
+            self.winOrLoseLabel.setText(f'Sorry, you guessed wrong!\nThe number was {number}')
+            balance = balance - 10
+            self.balanceCounter.setText(f'{balance}')
+            if balance < 1:
+                self.errorLabel.setText('You have a balance of 0\nClick reset to play again')
+
+    def reset(self, balance):
+        if balance == int(0):
+            balance == int(100)
+        else:
+            self.errorLabel.setText('Balance must be zero before you can reset.')
